730beefcbef0cb2f2982f5bb3fe909d112510f6e
SpanksMcYeet <xenogaming1426@gmail.com>
2022-01-19 22:37:02 -0600

    Literally nothing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,34 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        'bg-gradient': {
-          '0%, 100%': {
+        bggradient: {
+          '0%': {
             transform: 'rotate(-270deg)'
           },
-          '50%': {
+          '100%': {
             transform: 'rotate(270deg)'
+          },
+        },
+        spinInLeft: {
+          '0%': {
+            opacity: '0',
+            transform: 'rotateZ(0deg) scale3d(0, 0, 0)',
+          },
+          '50%': {opacity: '0.7'},
+          '70%': {
+            transform: 'rotateZ(-360deg) scale3d(1, 1, 1)',
+            opacity: '0.7',
+          },
+          '100%': {
+            transform: 'rotateZ(-360deg) scale3d(1, 1, 1)',
+            opacity: '0.7',
           },
         },
       },
       animation: {
-        'bg-gradient': 'bg-gradient 15s ease-in-out',
-        'bg-spin': 'spin 15s ease-in-out'
+        bggradient: 'bggradient 15s ease-in-out',
+        bgspin: 'spin 15s ease-in-out',
+        spinInLeft: 'spinInLeft 5s cubic-bezier(0.23, 1, 0.32, 1)'
       }
     },
   },
